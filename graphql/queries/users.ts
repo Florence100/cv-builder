cb0f428d6c94374735461218d3c79aad609f3b55
anastasia <anastasia.gribovskaia@gmail.com>
2026-06-17 11:19:00 +0300

    chore: add Apollo Client, GraphQL and integration for Next.js

---
+++ graphql/queries/users.ts
@@ -0,0 +1,12 @@
+// import { gql } from "@apollo/client";
+
+// example
+
+// export const GET_USERS = gql`
+//   query GetUsers {
+//     users {
+//       id
+//       name
+//     }
+//   }
+// `;
