3bac50dd36ddb8498ea85a97beaa840d112365a1
anastasia <anastasia.gribovskaia@gmail.com>
2026-06-18 09:56:31 +0300

    feat/auth: create base navigation between login and signup pages

---
+++ app/auth/layout.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+import { useTranslations } from 'next-intl';
+
+export default function AuthLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const t = useTranslations('AuthPage');
+
+  return (
+      <div className="flex flex-col gap-4">
+        <nav className='flex gap-2'>
+          <Link href="./login">{t('loginTab')}</Link>
+          <Link href="./signup">{t('signupTab')}</Link>
+        </nav>
+        {children}
+      </div>
+  );
+}
--- graphql/provider.tsx
+++ graphql/provider.tsx
@@ -9,7 +9,7 @@ import {
 
 function makeClient() {
   const httpLink = new HttpLink({
-    uri: process.env.PUBLIC_GRAPHQL_API_URL,
+    uri: process.env.VITE_GRAPHQL_URL,
     fetchOptions: {
       // Optional: Next.js-specific fetch options
       // Note: This doesn't work with `export const dynamic = "force-static"`
