914e081114cd8af6aba7fbb979d9617b216df5a7
Seva Kavalenka <seva.kavalenka@gmail.com>
2026-06-22 12:28:41 +0200

    styles: add ui for signup page

--- src/pages/signup/ui/SignupPage.tsx
+++ src/pages/signup/ui/SignupPage.tsx
@@ -17,16 +17,27 @@ export const SignupPage = () => {
           <Input
             type="email"
             placeholder={t('emailPlaceholder')}
-            className="h-12 p-3 text-base placeholder:text-base bg-transparent border-border rounded-none"
+            className="h-12 p-3 !text-base placeholder:text-base bg-transparent border-border rounded-none"
           />
           <Input
             type="password"
             placeholder={t('passwordPlaceholder')}
-            className="h-12 p-3 text-base placeholder:text-base bg-transparent border-border rounded-none"
+            className="h-12 p-3 !text-base placeholder:text-base bg-transparent border-border rounded-none"
           />
-          <div>
-            <Button />
-            <Button />
+          <div className="flex flex-col items-center gap-2 mt-15">
+            <Button
+              type="submit"
+              className="w-55 h-12 uppercase rounded-full shadow-md cursor-pointer "
+            >
+              {t('submitButton')}
+            </Button>
+            <Button
+              type="button"
+              variant="ghost"
+              className="w-55 h-12 text-transform: uppercase text-muted-foreground cursor-pointer"
+            >
+              {t('haveAccountButton')}
+            </Button>
           </div>
         </form>
       </div>
