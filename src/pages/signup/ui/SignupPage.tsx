b07fed7e24dc3abba1b8a957e1c60bdbf57a3f08
Seva Kavalenka <seva.kavalenka@gmail.com>
2026-06-22 08:11:45 +0200

    styles: set base font

--- src/pages/signup/ui/SignupPage.tsx
+++ src/pages/signup/ui/SignupPage.tsx
@@ -6,18 +6,29 @@ export const SignupPage = () => {
   const t = useTranslations('pages.signup');
 
   return (
-    <div>
-      <div>
-        <h1>{t('title')}</h1>
-        <p>{t('subtitle')}</p>
-        <div>
-          <Input />
-          <Input />
+    <div className="flex flex-1 justify-center items-center">
+      <div className="flex flex-col flex-1 max-w-[560] justify-center items-center">
+        <div className="flex flex-col w-full items-center mb-10 gap-6 text-foreground">
+          <h1 className="text-4xl">{t('title')}</h1>
+          <p>{t('subtitle')}</p>
         </div>
-        <div>
-          <Button />
-          <Button />
-        </div>
+
+        <form className="w-full flex flex-col gap-5">
+          <Input
+            type="email"
+            placeholder={t('emailPlaceholder')}
+            className="h-12 p-3 text-base placeholder:text-base bg-transparent border-border rounded-none"
+          />
+          <Input
+            type="password"
+            placeholder={t('passwordPlaceholder')}
+            className="h-12 p-3 text-base placeholder:text-base bg-transparent border-border rounded-none"
+          />
+          <div>
+            <Button />
+            <Button />
+          </div>
+        </form>
       </div>
     </div>
   );
