c2f1d88a299fdb181c433d5017946d714e892778
Seva Kavalenka <seva.kavalenka@gmail.com>
2026-06-23 12:18:40 +0200

    feat: add access token check (#13)
    * feat: add access token check
    
    * fix: fix a typo in the error message

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,10 +1,33 @@
+'use client';
+
+import { accessTokenVar } from '@/src/entities/session/model/session';
 import { AuthNavigation } from '@/src/widgets/auth-navigation';
+import { useReactiveVar } from '@apollo/client/react';
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
 
 export default function AuthLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const router = useRouter();
+  const accessToken = useReactiveVar(accessTokenVar);
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setIsMounted(true), 0);
+    return () => clearTimeout(timer);
+  }, []);
+
+  useEffect(() => {
+    if (accessToken) {
+      router.replace('/users');
+    }
+  }, [accessToken, router]);
+
+  if (!isMounted || accessToken) return null;
+
   return (
     <main className="flex flex-col flex-1 gap-4 w-[35rem] max-w-full">
       <AuthNavigation />
