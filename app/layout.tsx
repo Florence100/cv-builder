a772cd98e02f4dfc33eafd203edaad7c1c74ca78
anastasia <anastasia.gribovskaia@gmail.com>
2026-07-08 10:01:11 +0300

    fix: delete async function

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export const metadata: Metadata = {
   description: 'The app for building CVs',
 };
 
-export default async function RootLayout({
+export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
