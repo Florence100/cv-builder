ef0da9617664c4fedb70491f68040fc17a785a32
anastasia <anastasia.gribovskaia@gmail.com>
2026-07-10 13:10:46 +0300

    fix: type error

--- app/(dashboard)/cvs/[cvId]/projects/page.tsx
+++ app/(dashboard)/cvs/[cvId]/projects/page.tsx
@@ -1,10 +1,7 @@
 import { CvProjectsPage } from '@/src/pages/cv-projects';
-import { cookies } from 'next/headers';
 
 export default async function CvProjects({ params }: { params: Promise<{ cvId: string }> }) {
   const { cvId } = await params;
 
-  const cookieStore = await cookies();
-  const loggedInUserId = cookieStore.get('userId')?.value;
-  return <CvProjectsPage cvId={cvId} loggedInUserId={loggedInUserId} />;
+  return <CvProjectsPage cvId={cvId} />;
 }
