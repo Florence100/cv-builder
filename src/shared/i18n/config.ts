142de86b2c91cfe3fe7430cd15ff66bf9cb70e3d
Seva Kavalenka <seva.kavalenka@gmail.com>
2026-07-13 08:40:39 +0200

    feat: add more languages

--- src/shared/i18n/config.ts
+++ src/shared/i18n/config.ts
@@ -1,4 +1,7 @@
 export const SUPPORTED_LOCALES = [
   { value: 'en', label: 'English' },
   { value: 'ru', label: 'Русский' },
+  { value: 'be', label: 'Беларуская' },
+  { value: 'pl', label: 'Polski' },
+  { value: 'de', label: 'Deutsch' },
 ] as const;
